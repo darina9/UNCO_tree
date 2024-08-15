ef3f64322b0f616060898a26ef8061da2b996515
darina9 <111124490+darina9@users.noreply.github.com>
2024-08-15 12:59:32 +0200

    Update NinthComponent.vue

--- src/components/NinthComponent.vue
+++ src/components/NinthComponent.vue
@@ -3,73 +3,122 @@
     <div class="ninth-form">
       <form id="contact-form" @submit.prevent="submitForm">
         <div class="form-group">
-          <label for="contact-method" class="green-border">{{ $t('ninth.questions') }}</label>
+          <label for="contact-method" class="green-border">{{
+            $t("ninth.questions")
+          }}</label>
           <div class="dropdown">
-            <input type="text" id="contact-method" v-model="selectedMethod" :placeholder="t('ninth.method_of_communication')" readonly>
+            <input
+              type="text"
+              id="contact-method"
+              v-model="selectedMethod"
+              :placeholder="t('ninth.method_of_communication')"
+              readonly
+            />
             <button type="button" id="dropdown-btn" @click="toggleDropdown">
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 40 40"
+                fill="none"
+              >
                 <g clip-path="url(#clip0_402_104)">
-                  <path d="M4.44444 40C3.2657 40 2.13524 39.5317 1.30175 38.6983C0.46825 37.8648 0 36.7343 0 35.5556V4.44444C0 3.2657 0.46825 2.13524 1.30175 1.30175C2.13524 0.468253 3.2657 0 4.44444 0L35.5556 0C36.7343 0 37.8648 0.468253 38.6983 1.30175C39.5317 2.13524 40 3.2657 40 4.44444V35.5556C40 36.7343 39.5317 37.8648 38.6983 38.6983C37.8648 39.5317 36.7343 40 35.5556 40H4.44444Z" fill="#BC0051"/>
-                  <path d="M24.4449 7.77783V17.7778H32.2227L20.0004 32.2223L7.77821 17.7778H15.556V7.77783H24.4449Z" fill="#FEFFFA"/>
+                  <path
+                    d="M4.44444 40C3.2657 40 2.13524 39.5317 1.30175 38.6983C0.46825 37.8648 0 36.7343 0 35.5556V4.44444C0 3.2657 0.46825 2.13524 1.30175 1.30175C2.13524 0.468253 3.2657 0 4.44444 0L35.5556 0C36.7343 0 37.8648 0.468253 38.6983 1.30175C39.5317 2.13524 40 3.2657 40 4.44444V35.5556C40 36.7343 39.5317 37.8648 38.6983 38.6983C37.8648 39.5317 36.7343 40 35.5556 40H4.44444Z"
+                    fill="#BC0051"
+                  />
+                  <path
+                    d="M24.4449 7.77783V17.7778H32.2227L20.0004 32.2223L7.77821 17.7778H15.556V7.77783H24.4449Z"
+                    fill="#FEFFFA"
+                  />
                 </g>
                 <defs>
                   <clipPath id="clip0_402_104">
-                    <rect width="40" height="40" fill="white" transform="matrix(0 1 -1 0 40 0)"/>
+                    <rect
+                      width="40"
+                      height="40"
+                      fill="white"
+                      transform="matrix(0 1 -1 0 40 0)"
+                    />
                   </clipPath>
                 </defs>
               </svg>
             </button>
             <ul id="dropdown-list" v-show="dropdownVisible">
-              <li v-for="method in methods" :key="method.value" @click="selectMethod(method.value)">{{ method.text }}</li>
+              <li
+                v-for="method in methods"
+                :key="method.value"
+                @click="selectMethod(method.value)"
+              >
+                {{ method.text }}
+              </li>
             </ul>
           </div>
         </div>
 
         <div class="form-group">
-          <label for="contact-info">{{ }}</label>
-          <input type="text" id="contact-info" v-model="contactInfo" :placeholder="contactInfoPlaceholder" required :pattern="contactInfoPattern">
+          <label for="contact-info">{{}}</label>
+          <input
+            type="text"
+            id="contact-info"
+            v-model="contactInfo"
+            :placeholder="contactInfoPlaceholder"
+            required
+            :pattern="contactInfoPattern"
+          />
         </div>
 
         <div class="form-group" id="message-group">
-          <label for="message">{{ $t('ninth.message') }}</label>
-          <textarea id="message" v-model="message" :placeholder="t('ninth.enter_message')"></textarea>
+          <label for="message">{{ $t("ninth.message") }}</label>
+          <textarea
+            id="message"
+            v-model="message"
+            :placeholder="t('ninth.enter_message')"
+          ></textarea>
         </div>
 
         <div class="form-group">
-          <button type="submit">{{ $t('ninth.send') }}</button>
+          <button type="submit">{{ $t("ninth.send") }}</button>
         </div>
 
-        <div id="form-status" :style="{ display: formStatusVisible ? 'block' : 'none', color: formStatusColor }">{{ formStatus }}</div>
+        <div
+          id="form-status"
+          :style="{
+            display: formStatusVisible ? 'block' : 'none',
+            color: formStatusColor,
+          }"
+        >
+          {{ formStatus }}
+        </div>
       </form>
     </div>
-   
   </div>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
-import { useI18n } from 'vue-i18n';
+import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 
 export default {
-  name: 'NinthComponent',
+  name: "NinthComponent",
   setup() {
     const { t, locale } = useI18n();
-    const selectedMethod = ref('');
-    const contactInfo = ref('');
-    const contactInfoLabel = ref(t('ninth.enter_contact_info'));
-    const contactInfoPlaceholder = ref(t('ninth.enter_contact_info'));
-    const contactInfoPattern = ref('.*');
-    const message = ref('');
-    const formStatus = ref('');
+    const selectedMethod = ref("");
+    const contactInfo = ref("");
+    const contactInfoLabel = ref(t("ninth.enter_contact_info"));
+    const contactInfoPlaceholder = ref(t("ninth.enter_contact_info"));
+    const contactInfoPattern = ref(".*");
+    const message = ref("");
+    const formStatus = ref("");
     const formStatusVisible = ref(false);
-    const formStatusColor = ref('green');
+    const formStatusColor = ref("green");
     const dropdownVisible = ref(false);
 
     const methods = ref([
-      { value: 'Phone', text: t('ninth.phone_number') },
-      { value: 'Email', text: t('ninth.email') },
-      { value: 'Messenger', text: t('ninth.messenger_link') },
-      { value: 'Social', text: t('ninth.social_network_link') },
+      { value: "Phone", text: t("ninth.phone_number") },
+      { value: "Email", text: t("ninth.email") },
+      { value: "Messenger", text: t("ninth.messenger_link") },
+      { value: "Social", text: t("ninth.social_network_link") },
     ]);
 
     const toggleDropdown = () => {
@@ -77,34 +126,35 @@ export default {
     };
 
     const selectMethod = (method) => {
-      selectedMethod.value = methods.value.find(m => m.value === method).text;
+      selectedMethod.value = methods.value.find((m) => m.value === method).text;
       dropdownVisible.value = false;
 
       switch (method) {
-        case 'Phone':
-          contactInfoPlaceholder.value = '+49';
-          contactInfoPattern.value = '\\+\\d+';
+        case "Phone":
+          contactInfoPlaceholder.value = "+49";
+          contactInfoPattern.value = "\\+\\d+";
           break;
-        case 'Email':
-          contactInfoPlaceholder.value = 'example@example.com';
-          contactInfoPattern.value = '[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}';
+        case "Email":
+          contactInfoPlaceholder.value = "example@example.com";
+          contactInfoPattern.value =
+            "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
           break;
-        case 'Messenger':
-          contactInfoPlaceholder.value = t('ninth.paste_messenger_link');
-          contactInfoPattern.value = '.*';
+        case "Messenger":
+          contactInfoPlaceholder.value = t("ninth.paste_messenger_link");
+          contactInfoPattern.value = ".*";
           break;
-        case 'Social':
-          contactInfoPlaceholder.value = t('ninth.paste_social_network_link');
-          contactInfoPattern.value = '.*';
+        case "Social":
+          contactInfoPlaceholder.value = t("ninth.paste_social_network_link");
+          contactInfoPattern.value = ".*";
           break;
         default:
-          contactInfoPlaceholder.value = t('ninth.enter_contact_info');
-          contactInfoPattern.value = '.*';
+          contactInfoPlaceholder.value = t("ninth.enter_contact_info");
+          contactInfoPattern.value = ".*";
       }
     };
 
     const submitForm = async () => {
-      const form = document.getElementById('contact-form');
+      const form = document.getElementById("contact-form");
 
       if (!form.checkValidity()) {
         form.reportValidity();
@@ -112,45 +162,45 @@ export default {
       }
 
       const formData = new FormData();
-      formData.append('contact-method', selectedMethod.value);
-      formData.append('contact-info', contactInfo.value);
-      formData.append('message', message.value);
+      formData.append("contact-method", selectedMethod.value);
+      formData.append("contact-info", contactInfo.value);
+      formData.append("message", message.value);
 
       try {
-        const response = await fetch('https://feedback.foodfutures.net/', {
-          method: 'POST',
+        const response = await fetch("https://feedback.foodfutures.net/", {
+          method: "POST",
           body: formData,
           headers: {
-            'Accept': 'application/json'
-          }
+            Accept: "application/json",
+          },
         });
 
         if (response.ok) {
-          formStatus.value = 'Form submitted successfully!';
-          formStatusColor.value = 'green';
+          formStatus.value = "Form submitted successfully!";
+          formStatusColor.value = "green";
           formStatusVisible.value = true;
           setTimeout(() => {
             formStatusVisible.value = false;
           }, 5000);
         } else {
-          throw new Error('Form submission failed');
+          throw new Error("Form submission failed");
         }
       } catch (error) {
-        console.error('Error:', error);
-        formStatus.value = 'There was an error submitting the form.';
-        formStatusColor.value = 'red';
+        console.error("Error:", error);
+        formStatus.value = "There was an error submitting the form.";
+        formStatusColor.value = "red";
         formStatusVisible.value = true;
       }
     };
 
     watch(locale, () => {
-      contactInfoLabel.value = t('ninth.contact_info');
-      contactInfoPlaceholder.value = t('ninth.enter_contact_info');
+      contactInfoLabel.value = t("ninth.contact_info");
+      contactInfoPlaceholder.value = t("ninth.enter_contact_info");
       methods.value = [
-        { value: 'Phone', text: t('ninth.phone_number') },
-        { value: 'Email', text: t('ninth.email') },
-        { value: 'Messenger', text: t('ninth.messenger_link') },
-        { value: 'Social', text: t('ninth.social_network_link') },
+        { value: "Phone", text: t("ninth.phone_number") },
+        { value: "Email", text: t("ninth.email") },
+        { value: "Messenger", text: t("ninth.messenger_link") },
+        { value: "Social", text: t("ninth.social_network_link") },
       ];
     });
 
@@ -169,21 +219,20 @@ export default {
       methods,
       toggleDropdown,
       selectMethod,
-      submitForm
+      submitForm,
     };
-  }
+  },
 };
 </script>
 
-<style>
+<style scoped>
 @import url("../assets/common-styles.css");
 .ninth__component {
-  margin-top: 650px;
+  margin-top: 200px;
   border-radius: 20px 20px 0 0;
-  
+
   flex-wrap: wrap;
-  padding-top: 40px;
-  padding-bottom: 40px;
+  padding: 40px 10px;
   position: relative;
   width: 100%;
 }
@@ -208,8 +257,6 @@ export default {
 .svg-link {
   display: inline-block;
 }
-
-
 
 .ninth__contacts {
   display: flex;
@@ -262,13 +309,13 @@ export default {
 }
 
 label[for="contact-method"] {
-  color: #FEFFFA;
-font-family: Raleway;
-font-size: 24px;
-font-style: normal;
-font-weight: 700;
-line-height: normal;
-margin-bottom: 20px;
+  color: #fefffa;
+  font-family: Raleway;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  margin-bottom: 20px;
 }
 
 input[type="text"],
@@ -333,7 +380,7 @@ button[type="submit"] {
   width: 100%;
   /* border-radius: 8px; */
   box-sizing: border-box;
- border: none;
+  border: none;
   z-index: 10;
   max-height: 150px;
   color: #313131;
@@ -358,7 +405,7 @@ button[type="submit"] {
 }
 
 #dropdown-list li:hover {
-  background-color: #A2C33D;
+  background-color: #a2c33d;
   color: #fefffa;
 }
 
@@ -395,9 +442,9 @@ textarea:focus {
   width: 100%;
 }
 @media only screen and (max-width: 1439px) and (min-width: 768px) {
-
   .ninth__component {
-    margin-top: 200px;}
+    margin-top: 200px;
+  }
 }
 button[type="submit"] {
   width: 100%;
@@ -415,9 +462,11 @@ button[type="submit"] {
 }
 @media only screen and (max-width: 767px) {
   .ninth__component {
-  margin-top: 120px;
-padding: 0 10px;
-}
-
+    margin-top: 120px;
+  }
+  .ninth-form {
+    padding: 0 10px;
+  }
 }
 </style>
+
