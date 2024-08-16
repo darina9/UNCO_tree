7356406d3530d722a45dcaf59981b6613eab1425
darina9 <111124490+darina9@users.noreply.github.com>
2024-08-16 11:41:57 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
-  <router-view></router-view>
+  <router-view />
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+
 export default {
-  name: 'App',
+  setup() {
+    const router = useRouter();
+
+    if (!router.currentRoute.value.name) {
+      const lang = getCookie("language") || "en";
+      router.replace({ name: 'MainPage', params: { lang } });
+    }
+
+    function getCookie(name) {
+      const nameEQ = name + "=";
+      const ca = document.cookie.split(";");
+      for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+      }
+      return null;
+    }
+  }
 };
 </script>
-
-<style>
-@import url('./assets/common-styles.css');
-</style>
